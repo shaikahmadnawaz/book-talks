1a1e6ef94dc66ca328a06439347733ab5364d3af
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2023-03-20 15:44:11 +0530

    user schema added

---
+++ server/models/User.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+import validator from "validator";
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Username is required"],
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+    validate: {
+      validator: validator.isEmail,
+      message: "Please enter a valid email address",
+    },
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+    minlength: 5,
+  },
+});
+
+export default mongoose.model("User", UserSchema);
