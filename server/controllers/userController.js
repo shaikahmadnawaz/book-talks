966242a0fa89bc0875db111f8570dd695f039c65
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2023-04-29 20:10:43 +0530

    authentication working

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,7 +1,7 @@
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
-import User from "../models/User";
+import User from "../models/User.js";
 
 export const signup = async (req, res) => {
   const { name, email, password } = req.body;
@@ -27,7 +27,7 @@ export const login = async (req, res) => {
   const { email, password } = req.body;
 
   try {
-    const user = User.findOne({ email });
+    const user = await User.findOne({ email });
 
     if (!user) {
       return res.status(404).json({ message: "User not found" });
