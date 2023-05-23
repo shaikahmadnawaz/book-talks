2f0cdaa19ea41049d3695f830d1fa22b9b248137
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2023-05-24 00:58:29 +0530

    signup card completed

--- client/src/components/auth/SignupPage.js
+++ client/src/components/auth/SignupPage.js
@@ -1,7 +1,90 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { signupUser } from "../../redux/authSlice";
 
 const SignupPage = () => {
-  return <div>SignupPage</div>;
+  const dispatch = useDispatch();
+  const loading = useSelector((state) => state.auth.loading);
+  const error = useSelector((state) => state.auth.error);
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSignup = (e) => {
+    e.preventDefault();
+
+    // Dispatch the signupUser async thunk action
+    dispatch(signupUser({ name, email, password }));
+
+    // Clear the input fields
+    setName("");
+    setEmail("");
+    setPassword("");
+  };
+
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="bg-white p-8 rounded shadow w-96">
+        <h2 className="text-2xl font-bold mb-4">Sign Up</h2>
+        {error && <p className="text-red-500 mb-4">{error.message}</p>}
+        <form onSubmit={handleSignup}>
+          <div className="mb-4">
+            <label
+              htmlFor="name"
+              className="block text-gray-700 font-semibold mb-2"
+            >
+              Name
+            </label>
+            <input
+              type="text"
+              id="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              className="border border-gray-300 rounded px-3 py-2 w-full"
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="email"
+              className="block text-gray-700 font-semibold mb-2"
+            >
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="border border-gray-300 rounded px-3 py-2 w-full"
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="password"
+              className="block text-gray-700 font-semibold mb-2"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="border border-gray-300 rounded px-3 py-2 w-full"
+            />
+          </div>
+          <button
+            type="submit"
+            className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded disabled:bg-gray-400"
+            disabled={loading}
+          >
+            {loading ? "Signing up..." : "Sign Up"}
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default SignupPage;
