dfd53e8a00d1453aca16df27e08026d477cac49e
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2023-06-09 20:10:57 +0530

    maximum controllers completed as of now

--- server/controllers/bookController.js
+++ server/controllers/bookController.js
@@ -64,18 +64,22 @@ export const addBook = asyncHandler(async (req, res) => {
 export const updateBook = asyncHandler(async (req, res) => {
   const { title, author, description, coverImage } = req.body;
 
-  const book = await Book.findById(req.params.id);
+  try {
+    const book = await Book.findById(req.params.id);
 
-  if (book) {
-    book.title = title;
-    book.author = author;
-    book.description = description;
-    book.coverImage = coverImage;
+    if (book) {
+      book.title = title;
+      book.author = author;
+      book.description = description;
+      book.coverImage = coverImage;
 
-    const updatedBook = await book.save();
-    res.json(updatedBook);
-  } else {
-    res.status(404).json({ message: "Book not found" });
+      const updatedBook = await book.save();
+      res.json({ message: "Book updated" }, updatedBook);
+    } else {
+      res.status(404).json({ message: "Book not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Failed to update book" });
   }
 });
 
@@ -83,30 +87,39 @@ export const updateBook = asyncHandler(async (req, res) => {
 // @route   DELETE /api/books/:id
 // @access  Private
 export const deleteBook = asyncHandler(async (req, res) => {
-  const book = await Book.findById(req.params.id);
+  try {
+    const book = await Book.findById(req.params.id);
 
-  if (book) {
-    await book.remove();
-    res.json({ message: "Book removed" });
-  } else {
-    res.status(404).json({ message: "Book not found" });
+    if (book) {
+      await book.remove();
+      res.status(200).json({ message: "Book removed" });
+    } else {
+      res.status(404).json({ message: "Book not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Server Error" });
   }
 });
 
-export const getReviews = asyncHandler(async (req, res, next) => {
-  const bookId = req.params.bookId;
-  if (!bookId) {
-    return res.status(400).json({ message: "Book ID not received !" });
-  }
+// @desc    Get reviews for a book
+// @route   GET /api/books/:bookId/reviews
+// @access  Public
+export const getReviews = asyncHandler(async (req, res) => {
   try {
+    const bookId = req.params.bookId;
+    if (!bookId) {
+      return res.status(400).json({ message: "Book ID not received!" });
+    }
+
     const book = await Book.findById(bookId);
     if (!book) {
-      return res.status(404).json({ message: "Book Not Found !" });
+      return res.status(404).json({ message: "Book Not Found!" });
     }
+
     const reviews = await Book.findById(bookId).populate("reviews");
-    return res.status(200).json({ message: "reviews sent ", reviews });
-  } catch (err) {
-    return res.status(500).json({ message: err.message });
+    return res.status(200).json({ message: "Reviews sent", reviews });
+  } catch (error) {
+    return res.status(500).json({ message: "Server Error" });
   }
 });
 
@@ -125,7 +138,7 @@ export const addReview = asyncHandler(async (req, res) => {
         user: userId,
         book: req.params.id,
       });
-      return res.status(200).json({ message: "Review Created !" });
+      return res.status(200).json({ message: "Review Created!" }, newReview);
     }
     return res.status(404).json({ message: "Book Not Found !" });
   } catch (err) {
@@ -139,24 +152,28 @@ export const addReview = asyncHandler(async (req, res) => {
 export const updateReview = asyncHandler(async (req, res) => {
   const { text, rating } = req.body;
 
-  const book = await Book.findById(req.params.id);
+  try {
+    const book = await Book.findById(req.params.id);
 
-  if (book) {
-    const review = book.reviews.find(
-      (review) => review._id.toString() === req.params.reviewId.toString() // Add toString() for comparison
-    );
+    if (book) {
+      const review = book.reviews.find(
+        (review) => review._id.toString() === req.params.reviewId.toString()
+      );
 
-    if (review) {
-      review.text = text;
-      review.rating = rating;
+      if (review) {
+        review.text = text;
+        review.rating = rating;
 
-      await book.save();
-      res.json({ message: "Review updated" });
+        await book.save();
+        res.status(200).json({ message: "Review updated" });
+      } else {
+        res.status(404).json({ message: "Review not found" });
+      }
     } else {
-      res.status(404).json({ message: "Review not found" });
+      res.status(404).json({ message: "Book not found" });
     }
-  } else {
-    res.status(404).json({ message: "Book not found" });
+  } catch (error) {
+    res.status(500).json({ message: "Server Error" });
   }
 });
 
@@ -164,16 +181,29 @@ export const updateReview = asyncHandler(async (req, res) => {
 // @route   DELETE /api/books/:id/reviews/:reviewId
 // @access  Private
 export const deleteReview = asyncHandler(async (req, res) => {
-  const book = await Book.findById(req.params.id);
+  try {
+    const book = await Book.findById(req.params.id);
 
-  if (book) {
-    book.reviews = book.reviews.filter(
-      (review) => review._id.toString() !== req.params.reviewId.toString() // Add toString() for comparison
+    if (!book) {
+      return res.status(404).json({ message: "Book not found" });
+    }
+
+    const reviewId = req.params.reviewId;
+    const filteredReviews = book.reviews.filter(
+      (review) => review._id.toString() !== reviewId.toString()
     );
 
+    if (filteredReviews.length === book.reviews.length) {
+      return res.status(404).json({ message: "Review not found" });
+    }
+
+    book.reviews = filteredReviews;
     await book.save();
+
     res.json({ message: "Review removed" });
-  } else {
-    res.status(404).json({ message: "Book not found" });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Failed to delete review", error: error.message });
   }
 });
