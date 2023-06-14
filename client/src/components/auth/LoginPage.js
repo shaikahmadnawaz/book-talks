3199ffc0b0ab292498d5a3c7bc9d4c18b74037fe
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2023-06-14 19:18:29 +0530

    Improved login page UI

--- client/src/components/auth/LoginPage.js
+++ client/src/components/auth/LoginPage.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { loginUser } from "../../redux/authSlice";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { BsFillJournalBookmarkFill } from "react-icons/bs";
+import { ArrowRight } from "lucide-react";
 
 const LoginPage = () => {
   const dispatch = useDispatch();
@@ -9,7 +11,6 @@ const LoginPage = () => {
 
   const user = useSelector((state) => state.auth.user);
   const loading = useSelector((state) => state.auth.loading);
-  const error = useSelector((state) => state.auth.error);
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -37,51 +38,89 @@ const LoginPage = () => {
   }, [user, navigate]);
 
   return (
-    <div className="flex justify-center items-center h-screen">
-      <div className="bg-white p-8 rounded shadow w-96">
-        <h2 className="text-2xl font-bold mb-4">Login</h2>
-        {error && <p className="text-red-500 mb-4">{error.message}</p>}
-        <form onSubmit={handleLogin}>
-          <div className="mb-4">
-            <label
-              htmlFor="email"
-              className="block text-gray-700 font-semibold mb-2"
-            >
-              Email
-            </label>
-            <input
-              type="email"
-              id="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              className="border border-gray-300 rounded px-3 py-2 w-full"
-            />
+    <section>
+      <div className="flex items-center justify-center px-4 py-10 sm:px-6 sm:py-16 lg:px-8 lg:py-24">
+        <div className="xl:mx-auto xl:w-full xl:max-w-sm 2xl:max-w-md">
+          <div className="mb-2 flex justify-center">
+            <BsFillJournalBookmarkFill className="text-primary p-1" size={60} />
           </div>
-          <div className="mb-4">
-            <label
-              htmlFor="password"
-              className="block text-gray-700 font-semibold mb-2"
+          <h2 className="text-center text-2xl font-bold leading-tight text-black">
+            Sign in to your account
+          </h2>
+          <p className="mt-2 text-center text-sm text-gray-600 ">
+            Don&apos;t have an account?{" "}
+            <Link
+              to="/signup"
+              title=""
+              className="font-semibold text-black transition-all duration-200 hover:underline"
             >
-              Password
-            </label>
-            <input
-              type="password"
-              id="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              className="border border-gray-300 rounded px-3 py-2 w-full"
-            />
-          </div>
-          <button
-            type="submit"
-            className="bg-primary text-white font-semibold py-2 px-4 rounded disabled:bg-gray-400"
-            disabled={loading}
-          >
-            {loading ? "Logging in..." : "Login"}
-          </button>
-        </form>
+              Create a free account
+            </Link>
+          </p>
+          <form className="mt-8" onSubmit={handleLogin}>
+            <div className="space-y-5">
+              <div>
+                <label
+                  htmlFor="email"
+                  className="text-base font-medium text-gray-900"
+                >
+                  {" "}
+                  Email address{" "}
+                </label>
+                <div className="mt-2">
+                  <input
+                    className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1"
+                    type="email"
+                    placeholder="Email"
+                    id="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  ></input>
+                </div>
+              </div>
+              <div>
+                <div className="flex items-center justify-between">
+                  <label
+                    htmlFor=""
+                    className="text-base font-medium text-gray-900"
+                  >
+                    {" "}
+                    Password{" "}
+                  </label>
+                  {/* <Link
+                    href="#"
+                    title=""
+                    className="text-sm font-semibold text-black hover:underline"
+                  >
+                    {" "}
+                    Forgot password?{" "}
+                  </Link> */}
+                </div>
+                <div className="mt-2">
+                  <input
+                    className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1"
+                    type="password"
+                    placeholder="Password"
+                    id="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  ></input>
+                </div>
+              </div>
+            </div>
+            <div className="mt-8">
+              <button
+                type="submit"
+                disabled={loading}
+                className="inline-flex w-full items-center justify-center rounded-md bg-primary px-3.5 py-2.5 font-semibold leading-7 text-secondary"
+              >
+                Get started <ArrowRight className="ml-2" size={16} />
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
-    </div>
+    </section>
   );
 };
 
