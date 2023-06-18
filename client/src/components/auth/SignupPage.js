dd18a188fc474895800d8a856ba799dfc2313fb4
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2023-06-18 11:53:53 +0530

    still working on profile image

--- client/src/components/auth/SignupPage.js
+++ client/src/components/auth/SignupPage.js
@@ -25,13 +25,23 @@ const SignupPage = () => {
   const handleSignup = async (e) => {
     e.preventDefault();
 
+    const formData = new FormData();
+    formData.append("name", name);
+    formData.append("email", email);
+    formData.append("password", password);
+    if (profileImage) {
+      formData.append("profileImage", profileImage);
+    }
+
+    console.log(formData);
+
     // Dispatching the signupUser async thunk action
-    await dispatch(signupUser({ name, email, password, profileImage }));
+    await dispatch(signupUser(formData));
 
     // Clearing the input fields
-    setName("");
-    setEmail("");
-    setPassword("");
+    // setName("");
+    // setEmail("");
+    // setPassword("");
 
     // Checking if signup is successful
     const signUpSuccessful = !error;
@@ -46,32 +56,35 @@ const SignupPage = () => {
     <section>
       <div className="flex items-center justify-center px-4 py-10 sm:px-6 sm:py-16 lg:px-8 lg:py-24">
         <div className="xl:mx-auto xl:w-full xl:max-w-sm 2xl:max-w-md">
-          <div class="mb-2 flex justify-center">
+          <div className="mb-2 flex justify-center">
             <BsFillJournalBookmarkFill className="text-primary p-1" size={60} />
           </div>
-          <h2 class="text-center text-2xl font-bold leading-tight text-black">
+          <h2 className="text-center text-2xl font-bold leading-tight text-black">
             Sign up to create account
           </h2>
-          <p class="mt-2 text-center text-base text-gray-600">
+          <p className="mt-2 text-center text-base text-gray-600">
             Already have an account?{" "}
             <Link
               to="/login"
               title=""
-              class="font-medium text-black transition-all duration-200 hover:underline"
+              className="font-medium text-black transition-all duration-200 hover:underline"
             >
               Log In
             </Link>
           </p>
           <form onSubmit={handleSignup} className="mt-8">
-            <div class="space-y-5">
+            <div className="space-y-5">
               <div>
-                <label for="name" class="text-base font-medium text-gray-900">
+                <label
+                  htmlFor="name"
+                  className="text-base font-medium text-gray-900"
+                >
                   {" "}
                   Full Name{" "}
                 </label>
-                <div class="mt-2">
+                <div className="mt-2">
                   <input
-                    class="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+                    className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
                     type="text"
                     placeholder="Full Name"
                     id="name"
@@ -81,13 +94,16 @@ const SignupPage = () => {
                 </div>
               </div>
               <div>
-                <label for="email" class="text-base font-medium text-gray-900">
+                <label
+                  htmlFor="email"
+                  className="text-base font-medium text-gray-900"
+                >
                   {" "}
                   Email address{" "}
                 </label>
-                <div class="mt-2">
+                <div className="mt-2">
                   <input
-                    class="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+                    className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
                     type="email"
                     placeholder="Email"
                     id="email"
@@ -97,18 +113,18 @@ const SignupPage = () => {
                 </div>
               </div>
               <div>
-                <div class="flex items-center justify-between">
+                <div className="flex items-center justify-between">
                   <label
-                    for="password"
-                    class="text-base font-medium text-gray-900"
+                    htmlFor="password"
+                    className="text-base font-medium text-gray-900"
                   >
                     {" "}
                     Password{" "}
                   </label>
                 </div>
-                <div class="mt-2">
+                <div className="mt-2">
                   <input
-                    class="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+                    className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
                     type="password"
                     placeholder="Password"
                     id="password"
@@ -137,7 +153,7 @@ const SignupPage = () => {
                 <button
                   type="submit"
                   disabled={loading}
-                  class="inline-flex w-full items-center justify-center rounded-md bg-primary px-3.5 py-2.5 font-semibold leading-7 text-white"
+                  className="inline-flex w-full items-center justify-center rounded-md bg-primary px-3.5 py-2.5 font-semibold leading-7 text-white"
                 >
                   Create Account <ArrowRight className="ml-2" size={16} />
                 </button>
