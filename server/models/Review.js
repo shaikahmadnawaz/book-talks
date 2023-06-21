f36d48cff5755027bdf0e42c29a3cbd402880702
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2023-06-21 11:33:56 +0530

    working on review profile images

--- server/models/Review.js
+++ server/models/Review.js
@@ -1,26 +1,42 @@
 import mongoose from "mongoose";
 
-const ReviewSchema = new mongoose.Schema({
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
+const ReviewSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    book: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Book",
+    },
+    comment: {
+      type: String,
+      // required: true,
+    },
+    rating: {
+      type: Number,
+      // required: true,
+    },
+    date: {
+      type: Date,
+      default: Date.now,
+    },
   },
-  book: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Book",
-  },
-  comment: {
-    type: String,
-    // required: true,
-  },
-  rating: {
-    type: Number,
-    // required: true,
-  },
-  date: {
-    type: Date,
-    default: Date.now,
-  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    toObject: {
+      virtuals: true,
+    },
+  }
+);
+
+ReviewSchema.virtual("reviewer", {
+  ref: "User",
+  localField: "user",
+  foreignField: "_id",
 });
 
 export default mongoose.model("Review", ReviewSchema);
