4e2477a963ea6a81ce9ab6ae5b183a66a13fa2a8
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2023-06-23 00:47:40 +0530

    small bug fixed

--- client/src/components/books/User.js
+++ client/src/components/books/User.js
@@ -17,7 +17,7 @@ const User = ({ user }) => {
 
       <h3 className="text-lg font-semibold mb-2">Books:</h3>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        {user.books.map((book) => (
+        {user.books?.map((book) => (
           <div
             key={book.id}
             className="bg-gray-100 p-4 rounded-lg shadow-md flex items-center"
