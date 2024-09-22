482fd554bd004b30e2d17bef458ea3494d2aa101
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2024-09-22 19:33:57 +0530

    build(vercel): redeployed from cyclic to vercel

--- client/src/config/url.js
+++ client/src/config/url.js
@@ -1,3 +1,5 @@
 // export const BASE_URL = "http://localhost:5000";
 
-export const BASE_URL = "https://booktalks.cyclic.app";
+export const BASE_URL = "https://book-talks-api.vercel.app";
+
+// export const BASE_URL = "https://booktalks.cyclic.app";
